1df302bd589abe7624df7507a3b43062c8cdd7e4
ESCG\521013 <521013@chrome.escg.ac.uk>
2021-09-23 15:03:24 +0100

    w3schools up to data types

---
+++ W3SCHOOLS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace W3SCHOOLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I will print on another line.");
+            //Comments are signified by //
+            /* Multi Line Comments are signified by 
+             These*/
+            string name = "Joe";
+            Console.WriteLine(name);
+            int myNum = 15;
+            Console.WriteLine(myNum);
+            myNum = 2;
+            Console.WriteLine(myNum);
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+            string lastname = "mama";
+            Console.WriteLine(name + lastname);
+            string fullname = name + lastname;
+            Console.WriteLine(fullname);
+            int x = 4;
+            int y = 2;
+            Console.WriteLine(x * y);
+            
+        }
+    }
+}
